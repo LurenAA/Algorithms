2fc04a90dba8b3252951a26ea7a7ccc580ed2d80
nurenaa <978601499@qq.com>
2020-01-08 00:32:48 +0800

    update .gitignore

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ele/AFE
-# Build directory: /home/ele/AFE
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
