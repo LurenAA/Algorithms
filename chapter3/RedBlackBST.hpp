839efd05b58e4baf185e08f12b18d061d29341f1
nurenaa <978601499@qq.com>
2020-01-21 16:15:13 +0800

    添加红黑树的全部测试

--- chapter3/RedBlackBST.hpp
+++ chapter3/RedBlackBST.hpp
@@ -37,20 +37,27 @@ class RedBlackBST{
     void put(const Key& key, const Val& val);
     shared_ptr<Node> get(const Key& key) const;
     void deleteMin();
-    // void deleteMax();
-    // void delet(const Key&);
+    void deleteMax();
+    void delet(const Key&);
+    Key min() const;
+    Key max() const;
+    vector<Key> keys(const Key& m, const Key& e) const;
+    vector<Key> keys() const;
 
     const int size() const {return root ? root->N : 0;}
   private:
     
     shared_ptr<Node> root;
 
+    void keys(shared_ptr<Node> h, vector<Key>& res, const Key& m, const Key& e) const;
+    shared_ptr<Node> max(shared_ptr<Node> h) const;
+    shared_ptr<Node> min(shared_ptr<Node>) const;
     shared_ptr<Node> get(shared_ptr<Node> h,const Key& key) const;
-    // shared_ptr<Node> delet(shared_ptr<Node>, const Key&);
+    shared_ptr<Node> delet(shared_ptr<Node>, const Key&);
     shared_ptr<Node> balance(shared_ptr<Node>);
     shared_ptr<Node> removeRedLeft(shared_ptr<Node>);
-    // shared_ptr<Node> removeRedRight(shared_ptr<Node>);
-    // shared_ptr<Node> deleteMax(shared_ptr<Node>);
+    shared_ptr<Node> removeRedRight(shared_ptr<Node>);
+    shared_ptr<Node> deleteMax(shared_ptr<Node>);
     shared_ptr<Node> deleteMin(shared_ptr<Node>);
     shared_ptr<Node> put(shared_ptr<Node> h, const Key& key, const Val& val);
     void flipColor(shared_ptr<Node> h);
@@ -203,12 +210,13 @@ shared_ptr<typename RedBlackBST<Key,Val>::Node>
 RedBlackBST<Key,Val>::deleteMin(shared_ptr<Node> h) 
 {
   if(!h->left) 
-    return nullptr;
+    return h->right;
   if(!isRed(h->left) && !isRed(h->left->left))
   {
     h = removeRedLeft(h);
   }
   h->left = deleteMin(h->left);
+  h->N = Node::Nsize(h->left) + Node::Nsize(h->right) + 1;
   return balance(h);
 }
 
@@ -217,7 +225,8 @@ shared_ptr<typename RedBlackBST<Key,Val>::Node>
 RedBlackBST<Key,Val>::removeRedLeft(shared_ptr<Node> h) 
 {
   flipColor(h);
-  if(isRed(h->right) && isRed(h->right->left)) {
+  if(isRed(h->right) && isRed(h->right->left)) 
+  {
     h->right = rotateRight(h->right);
     h = rotateLeft(h);
     flipColor(h);
@@ -236,4 +245,150 @@ RedBlackBST<Key,Val>::balance(shared_ptr<Node> h)
   if(isRed(h->left) && isRed(h->right)) {
     flipColor(h);
   }
+  return h;
+}
+
+template<typename Key, typename Val>
+Key
+RedBlackBST<Key,Val>::min() const
+{
+  if(!root)
+    return Val();
+  return min(root)->key;
+}
+
+template<typename Key, typename Val>
+shared_ptr<typename RedBlackBST<Key,Val>::Node> 
+RedBlackBST<Key,Val>::min(shared_ptr<Node> h) const 
+{
+  if(!h->left)
+    return h;
+  return min(h->left);
+}
+
+template<typename Key, typename Val>
+Key 
+RedBlackBST<Key,Val>::max() const
+{
+  if(!root) 
+    return Val();
+  return max(root)->key;
+}
+
+template<typename Key, typename Val>
+shared_ptr<typename RedBlackBST<Key,Val>::Node> 
+RedBlackBST<Key,Val>::max(shared_ptr<Node> h) const
+{
+  if(!h->right)
+    return h;
+  return max(h->right);
+}
+
+template<typename Key, typename Val>
+void RedBlackBST<Key,Val>::deleteMax()
+{
+  if(!isRed(root->left) && !isRed(root->right))
+    root->color = Color::Red;
+  root = deleteMax(root);
+  if(root)
+    root->color = Color::Black;
+}
+
+template<typename Key, typename Val>
+shared_ptr<typename RedBlackBST<Key,Val>::Node> 
+RedBlackBST<Key,Val>::deleteMax(shared_ptr<Node> h)
+{
+  if(isRed(h->left))
+    h = rotateRight(h);
+  if(!h->right)
+    return h->left;
+  if(!isRed(h->right) && !isRed(h->right->left))
+    h = removeRedRight(h);
+  h->right = deleteMax(h->right);
+  h->N = Node::Nsize(h->left) + Node::Nsize(h->right) + 1;
+  return balance(h);
+}
+
+template<typename Key, typename Val>
+shared_ptr<typename RedBlackBST<Key,Val>::Node> 
+RedBlackBST<Key,Val>::removeRedRight(shared_ptr<Node> h)
+{
+  flipColor(h);
+  if(isRed(h->left->left)) {
+    h = rotateRight(h);
+    flipColor(h);
+  }
+  return h;
+}
+
+template<typename Key, typename Val>
+void RedBlackBST<Key,Val>::delet(const Key& key)
+{
+  if(!isRed(root->left) && !isRed(root->right))
+    root->color = Color::Red;
+  root = delet(root, key);
+  if(root)
+    root->color = Color::Black;
+}
+
+template<typename Key, typename Val>
+shared_ptr<typename RedBlackBST<Key,Val>::Node> 
+RedBlackBST<Key,Val>::delet(shared_ptr<Node> h, const Key& key) 
+{
+  int cmp = h->compareTo(key);
+  if(cmp > 0) {
+    if(!isRed(h->left) && !isRed(h->left->left)) 
+      h = removeRedLeft(h);
+    h->left = delet(h->left, key);
+  } else if(cmp < 0){
+    if(isRed(h->left))
+      h = rotateRight(h);
+    if(!isRed(h->right) && !isRed(h->right->left)) {
+      h = removeRedRight(h);
+    }
+    h->right = delet(h->right, key);
+  } else {
+    if(!h->right)
+      return h->left;
+    h->val = get(h->right, min(h->right)->key)->val;
+    h->key = min(h->right)->key;
+    h->right = deleteMin(h->right);
+  }
+  h->N = Node::Nsize(h->left) + Node::Nsize(h->right) + 1;
+  return balance(h);
+}
+
+template<typename Key, typename Val>
+vector<Key> 
+RedBlackBST<Key,Val>::keys(const Key& m, const Key& e) const
+{
+  vector<Key> res;
+  keys(root, res, m, e);
+  return res;
+}
+
+template<typename Key, typename Val>
+void 
+RedBlackBST<Key,Val>::keys(shared_ptr<Node> h, vector<Key>& res, const Key& m, const Key& e) const
+{
+  if(!h)
+    return ;
+  int cmp1 = h->compareTo(m),
+    cmp2 = h->compareTo(e);
+  if(cmp1 >= 0 && cmp2 <= 0) {
+    res.push_back(h->key);
+  }
+  if(cmp1 > 0) {
+    keys(h->left, res, m, e);
+  } 
+  if(cmp2 < 0) {
+    keys(h->right, res, m, e);
+  }
+}
+
+template<typename Key, typename Val>
+vector<Key> 
+RedBlackBST<Key,Val>::keys() const
+{
+  return keys(min(), max());
 }
--- chapter3/test2.cpp
+++ chapter3/test2.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 /**
  * 红黑树单元测试
- **/ 
+ **/
 
 TEST_CASE("put", "[rdBST]") {
   RedBlackBST<char, char> bst;
@@ -25,6 +25,90 @@ TEST_CASE("put", "[rdBST]") {
       bst.put(iv1[k], iv1[k]);
       REQUIRE(bst.size() == k + 1);
     }
+
+    SECTION("deleteMin") {
+      bst.deleteMin();
+      REQUIRE(bst.size() == 9);
+      REQUIRE(bst.min() == 'C');
+      bst.deleteMin();
+      REQUIRE(bst.size() == 8);
+      REQUIRE(bst.min() == 'E');
+      bst.deleteMin();
+      REQUIRE(bst.size() == 7);
+      REQUIRE(bst.min() == 'H');
+      bst.deleteMin();
+      REQUIRE(bst.size() == 6);
+      REQUIRE(bst.min() == 'L');
+      bst.deleteMin();
+      REQUIRE(bst.size() == 5);
+      REQUIRE(bst.min() == 'M');
+      bst.deleteMin();
+      REQUIRE(bst.size() == 4);
+      REQUIRE(bst.min() == 'P');
+      bst.deleteMin();
+      REQUIRE(bst.size() == 3);
+      REQUIRE(bst.min() == 'R');
+      bst.deleteMin();
+      REQUIRE(bst.size() == 2);
+      REQUIRE(bst.min() == 'S');
+      bst.deleteMin();
+      REQUIRE(bst.size() == 1);
+      REQUIRE(bst.min() == 'X');
+      bst.deleteMin();
+      REQUIRE(bst.size() == 0);
+    }
+
+    SECTION("deleteMax") {
+      bst.deleteMax();
+      REQUIRE(bst.max() == 'S');
+      REQUIRE(bst.size() == 9);
+      bst.deleteMax();
+      REQUIRE(bst.size() == 8);
+      REQUIRE(bst.max() == 'R');
+      bst.deleteMax();
+      REQUIRE(bst.size() == 7);
+      REQUIRE(bst.max() == 'P');
+      bst.deleteMax();
+      REQUIRE(bst.size() == 6);
+      REQUIRE(bst.max() == 'M');
+      bst.deleteMax();
+      REQUIRE(bst.size() == 5);
+      REQUIRE(bst.max() == 'L');
+      bst.deleteMax();
+      REQUIRE(bst.size() == 4);
+      REQUIRE(bst.max() == 'H');
+      bst.deleteMax();
+      REQUIRE(bst.size() == 3);
+      REQUIRE(bst.max() == 'E');
+      bst.deleteMax();
+      REQUIRE(bst.size() == 2);
+      REQUIRE(bst.max() == 'C');
+      bst.deleteMax();
+      REQUIRE(bst.size() == 1);
+      REQUIRE(bst.max() == 'A');
+      bst.deleteMax();
+      REQUIRE(bst.size() == 0);
+    }
+
+    SECTION("delete") {
+      bst.delet('X');
+      auto v1 = bst.keys();
+      REQUIRE(bst.size() == 9);
+      REQUIRE(bst.max() == 'S');
+      REQUIRE(v1.size() == 9);
+      bst.delet('A');
+      REQUIRE(bst.size() == 8);
+      REQUIRE(bst.min() == 'C');
+    }
+
+    SECTION("delete1") {
+      bst.delet('M');
+      REQUIRE(bst.size() == 9);
+      REQUIRE(bst.get('M') == nullptr);
+      bst.delet('E');
+      REQUIRE(bst.size() == 8);
+      REQUIRE(bst.get('E') == nullptr);
+    } 
   }
   SECTION("test every step") {
     bst.put('S','S');
